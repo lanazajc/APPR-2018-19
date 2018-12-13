3617965e041fb7b1338152ac37e9f9d52290a0a0
Lana Zajc <lana.zajc@gmail.com>
2018-12-13 09:55:19 +0100

    Spravila prvo tabelo v tidy data

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,7 +1,5 @@
 # 2. faza: Uvoz podatkov
-library(readr)
-library(reshape2)
-library(dplyr)
+
 
 # Uvoz 1. tabela: plače glede gospodarsko dejavnost
 
@@ -9,23 +7,34 @@ library(dplyr)
 
 stolpci <- c("Dejavnost", "Izobrazba", "Spol", 2010:2015)
 
-place_dejavnost_izobrazba <- read.csv2("dejavnost_izob.csv", 
-                                       header= FALSE, 
-                                       skip=3, na=c("", "-", " "))
+place_dejavnost_izobrazba <- read_csv2("podatki/dejavnost_izob.csv", 
+                                       col_names = stolpci,
+                                       skip=4, na=c("", "-", " "),
+                                       locale=locale(encoding="Windows-1250"))
+
+place_dejavnost_izobrazba <- place_dejavnost_izobrazba  %>% fill(1:2) %>% drop_na(3) %>%
+  melt(id.vars=stolpci[1:3], variable.name="Leto", value.name="Placa") %>%
+  mutate(Leto=parse_number(Leto))
            
 
 
 
 # Uvoz 2. tabela: 
 
-regija_starost <- read.csv2("regija_leto.csv", col.names = c("Regija", "Starost", 2010:2015),skip=3, header = FALSE)
+regija_starost <- read.csv2("podatki/regija_leto.csv", col.names = c("Regija", "Starost", 2010:2015),skip=3, header = FALSE)
 
+regija_starost <- regija_starost %>% fill(1)
 
 
 
 
 # Uvoz 3. tabela: 
 
+min_place <- read_html("podatki/minimalne_place.htm") %>% html_node(xpath="//table[@class='infoData']") %>%
+  html_table() %>% melt(id.vars="timegeo", variable.name="leto", value.name="placa") %>%
+  mutate(leto=parse_number(leto),
+         placa=parse_number(placa, na=c(":", ":(z)"), locale=locale(decimal_mark=".", grouping_mark=","))) %>%
+  drop_na(placa) 
 
 
 
