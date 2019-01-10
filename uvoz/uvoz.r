b5d3a679bbd02d2cf398e6d6c0ca0a29d1d74533
Lana Zajc <lana.zajc@gmail.com>
2019-01-10 11:57:43 +0100

    Grafa razliki v BDP in plači po državah. Uredila tabele

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,9 +1,5 @@
-# 2. faza: Uvoz podatkov
 
-
-# Uvoz 1. tabela: plače glede gospodarsko dejavnost
-
-
+# UVOZ 1. TABELE: plače glede gospodarsko dejavnost
 
 stolpci <- c("Dejavnost", "Izobrazba", "Spol", 2010:2015)
 
@@ -12,21 +8,20 @@ place_dejavnost_izobrazba <- read_csv2("podatki/dejavnost_izob.csv",
                                        skip=4, na=c("", "-", " "),
                                        locale=locale(encoding="Windows-1250"))
 
+place_dejavnost_izobrazba <- place_dejavnost_izobrazba  %>% fill(1:2) %>% drop_na(3) %>%
+  melt(id.vars=stolpci[1:3], variable.name="Leto", value.name="Placa") 
+
 # podatki/ pred imenom datoteke, ker mamo za directory nastavljen na glavno mapo
 #col_names nastavimo imena stolpcev
 #skip 4 - izpusti prve 4 vrstice
 #znake ""," " in "-" nadomesti z NA, da lahko v nadaljevanju uporabimo drop_na
-
-place_dejavnost_izobrazba <- place_dejavnost_izobrazba  %>% fill(1:2) %>% drop_na(3) %>%
-  melt(id.vars=stolpci[1:3], variable.name="Leto", value.name="Placa") 
-  
 
 # mutate(Leto=parse_number(Leto)) izbrisala ukaz ker ni deloval
 #fill zapolne prazne vrstice v stolpcu z enako vrednostjo kot v zgornji vrstici, vse do naslednjega polnega polja
 #drop_na izpusti/izbriše vse vrstice kjer imamo NA (argument je stolpce kjer išče NA)
 #melt spremeni prejšnje stolpce ( 2010:2015 v en stolpec z letom in enim s plačo)
 
-# Uvoz 2. tabela: 
+# UVOZ 2. TABELE: Plače po regijah in starosti 2010-2014 
 
 stolpci2 <- c("Regija", "Starost", 2010:2015)
 
@@ -36,10 +31,15 @@ regija_starost <- read.csv2("podatki/regija_leto.csv", col.names = stolpci2,skip
 names(regija_starost)[3:8] <- c("2010", "2011", "2012", "2013", "2014", "2015")
 
 regija_starost <- regija_starost %>% fill(1) %>% drop_na(2) %>% melt(id.vars=stolpci2[1:2], 
-                                variable.name="Leto", value.name = "Placa") 
-  
+                  variable.name="Leto", value.name = "Placa") 
+regija_starost <- regija_starost[!(regija_starost$Starost=="65 let ali več"), ]
+regija_starost <- regija_starost[!(regija_starost$Leto==2015), ]
 
-# Uvoz 3. tabela: 
+povp_starost <- regija_starost %>% filter(Starost=="15-64 let")
+
+sprememba <- povp_starost %>% filter(Leto==2010 | Leto==2014)
+
+# UVOZ 3. TABELA: Minimalne plače po državah
 
 min_place <- read_html("podatki/minimalne_place.htm") %>% html_node(xpath="//table[@class='infoData']") %>% 
   html_table() %>% melt(id.vars="timegeo", variable.name="leto", value.name="placa") %>%
@@ -48,7 +48,7 @@ min_place <- read_html("podatki/minimalne_place.htm") %>% html_node(xpath="//tab
 names(min_place)[1:3] <- c("Drzava", "Leto", "Placa")
 
 
-# Uvoz 4. tabela: BDP 
+# UVOZ 4. TABELE:  BDP po državah
 
 stolpci_bdp <- c("Leto", "Drzava", "neki", "neki2", "BDP")
 bdp <- read_csv("podatki/bdp.csv", col_names =stolpci_bdp, skip=1, na=c(":"))
@@ -57,11 +57,8 @@ bdp$neki2 <- NULL
 bdp <- bdp[c(2,1,3)]
 bdp <- bdp %>% drop_na(3)
 
-# Risanje grafov
 
-g_min_place <- ggplot(min_place, aes(Leto,Placa)) + geom_bar(stat = "identity", aes(fill = Drzava)) + xlab("Leto") + ylab("Placa") + ggtitle("Minimalna plača po državah") 
-print(g_min_place)
-#Združila tabeli BDP in min_place po Drzavah
+#Združila tabeli BDP in min_place po Drzavah za vsa leta 2004-2015
 
 min_place$Leto <- parse_integer(as.character(min_place$Leto))
 primerjava <- inner_join(min_place, bdp, by=c("Drzava", "Leto"))
@@ -80,31 +77,28 @@ names(l2015)[2:3] <- c("Placa v 2015", "BDP v 2015")
 #Tabela s podatki razlike v plači in BDP 2004, 2015:
   
 analiza1 <- inner_join(l2004, l2015, by=c("Drzava")) 
+analiza1 <- analiza1[!(analiza1$Drzava=="United States"), ]
 analiza1 <- analiza1 %>% melt(id.vars='Drzava', variable.name=('Placa'), value.name =('Vrednost')) %>%
   separate(Placa, c("Namen", "Leto"), " v ") %>% mutate(Leto=parse_number(as.character(Leto)))
-#analiza1 <- analiza1[order(analiza1$Drzava),]
 
-  
+
+#analiza1 <- analiza1[order(analiza1$Drzava),]
 #g_analiza1 <- ggplot(analiza1, aes(Drzava, Vrednost)) + geom_bar(stat = "identity") + xlab("Podatki") + ylab("Vrednost")
 #print(g_analiza1)
 
-visina <- table(analiza1$Placa, analiza1$Placa)
-lala <- analiza1 %>% group_by(Placa)
 
-g_sprememba_plac <- barplot(visina, main="Višina plač v 2004 in  2015 po državah",
-                            xlab="Država", col=c("darkblue","red"),
-                            legend = rownames(visina), beside=TRUE)
-
-#analiza1$Leto <- gsub("\\D", "", analiza1$Placa)
-#analiza1$Namen <- gsub("\\s.*", "", analiza1$Placa)
-#analiza1$Placa <- NULL
-gr <- analiza1 %>% filter(Namen == "Placa" )
+graf_place <- analiza1 %>% filter(Namen == "Placa" )
+graf_bdp <- analiza1 %>% filter(Namen == "BDP") %>% mutate(Vrednost=parse_number(as.character(Vrednost)))
 
 #analiza1$Razlika_Place <- ((analiza1[4]-analiza1[2])*100)/analiza1[2]
 #analiza1$Razlika_BDP <- analiza1[5]/analiza1[3]
 #names(analiza1)[6:7] <- c("Faktor razlike v plači", "Faktor razlike v BDP")                                   
 
-ggplot(gr, aes(x=Drzava, y=Vrednost, fill=factor(Leto))) + geom_col(position="dodge") + coord_flip() +
-  guides(fill=guide_legend("Leto")) + xlab("Država")
 
+# RISANJE GRAFOV: RAZLIKA V BDP IN PLAČI V 2004 IN 2015
 
+ggplot(graf_place, aes(x=Drzava, y=Vrednost, fill=factor(Leto))) + geom_col(position="dodge") + coord_flip() +
+  guides(fill=guide_legend("Leto")) + xlab("Država") + ggtitle("Plaže za leto 2004 in 2015 po državah")
+
+ggplot(graf_bdp, aes(x=Drzava, y=Vrednost, fill=factor(Leto))) + geom_col(position="dodge") + coord_flip() +
+  guides(fill=guide_legend("Leto")) + xlab("Država") + ggtitle("BDP za leto 2004 in 2015 po državah") 
