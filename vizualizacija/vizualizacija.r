15d3418d5ccc7be1eafe59a52ffce011a20f8ef2
lanazajc <lana.zajc@gmail.com>
2019-01-16 16:08:44 +0100

    Uvozila delujoč zemljevid Slovenije.

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,14 +1,22 @@
 # 3. faza: Vizualizacija podatkov
 
-zemljevid_regij <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/gpkg/gadm36_SVN_gpkg.zip", "gadm36_SVN_gpkg", pot.zemljevida ="gadm36_SVN_gpkg",
-                                   encoding="Windows-1250")
+zemljevid_slovenije <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
+                                       "SVN_adm1", mapa = "zemljevid_Slovenije", encoding = "Windows-1250") %>% fortify()
 
+ggplot() + geom_polygon(data=zemljevid_slovenije, aes(x=long, y=lat, group=group, fill=id)) +
+  guides(fill=FALSE) + labs(title ="Regije Slovenije")
 
+# 
+Regije <- unique(zemljevid_slovenije$NAME_1)
+Regije <- as.data.frame(regije, stringsAsFactors=FALSE)
 
+visina.place <- povp_starost %>% filter(Leto==2014)
+visina.place <- visina.place[c(-2,-3)]
 
+#združevanje <- left_join(visina.place, Regije, by="Regija")
 
-
-
+#zemljevid_slovenije$NAME_1 <- factor(zemljevid_slovenije$NAME_1, levels=levels(povp_starost$Regija))
+#zemljevid_slovenije <- fortify(zemljevid_slovenije)
 
 # Uvozimo zemljevid.
 zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
