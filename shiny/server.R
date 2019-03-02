8883197167bf86f78ee2520f9c257bbdf6663c55
lanazajc <lana.zajc@gmail.com>
2019-03-03 00:21:10 +0100

    Dokončan Shiny

--- shiny/server.R
+++ shiny/server.R
@@ -2,17 +2,13 @@ library(shiny)
 
 function(input, output) {
   
-  output$dejavnosti <- renderPlot({
+  output$graf_dejavnosti <- renderPlot({
     graf_dejavnosti <- ggplot(place_dejavnosti %>% filter(Leto == input$Leto)) + aes(x=Dejavnost, y=Plača, color=Spol) +
       geom_col(position = "dodge") + guides(fill=guide_legend("Leto")) +
       labs(title = "Plače po dejavnosti in spolu") + theme(plot.title = element_text(hjust = 0.5)) +
-      ylab("Plača") + xlab("Dejavnosti")
+      ylab("Plača") + xlab("Dejavnosti") + coord_flip()
     print(graf_dejavnosti)
   })
-  
- 
-  
-  
   
 }
 
