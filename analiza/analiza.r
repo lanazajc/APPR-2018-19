c40ba506dfdc955596a630f1b68aa93ee1e9b489
lanazajc <lana.zajc@gmail.com>
2019-03-13 17:30:28 +0100

    Regresija - napoved povišanja plač

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,16 @@
-# 4. faza: Analiza podatkov
+# 4. faza: Napredna analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
+#NAPOVED RASTI BRUTO MINIMALNE PLAČE V SLOVENIJI
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+slo_napoved <- min_place %>% filter(Drzava == 'Slovenia')
+prileganje <- lm(data = slo_napoved, Placa ~ Leto)
+g <- data.frame(Leto=seq(2016, 2020, 1))
+napoved <- mutate(g, Placa=predict(prileganje, g))
+
+graf_regresija <- ggplot(slo_napoved, aes(x=Leto, y=Placa)) + 
+  geom_point() + geom_smooth(method=lm, fullrange = TRUE, color = 'blue') + 
+  geom_point(data=napoved, aes(x=Leto, y=Placa), color='red', size=2) +
+  ggtitle('Napoved nadaljne rasti minimalne bruto plače v Sloveniji')
+
+
+
